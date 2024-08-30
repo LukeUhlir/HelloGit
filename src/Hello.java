574098b413c4945d8fb36adc061ad09ac38d9ac7
lukeu <lukeu@ChekhovsGun.unl.edu>
2024-08-30 16:09:41 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,18 @@
+/**
+	 * Author: Luke Uhlir (luhlir4@unl.edu) 
+	 * Date: 08/27/2024
+	 * 
+	 * A simple "Hello World!" program in Java
+	 * 
+	 * @author Luke Uhlir
+	 *
+	 */
+
+public class Hello {
+	
+		public static void main(String args[]) {
+			System.out.println("Luke Uhlir");
+		}
+	}
+
+}
